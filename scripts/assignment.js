a0f6df93f520a58669be6ab15bec2a05f829998f
Rudra <rudra.sondhi2@gmail.com>
2021-03-09 22:26:29 -0500

    6

---
+++ scripts/assignment.js
@@ -0,0 +1,44 @@
+//function for button 1
+function f1(){
+    alert("Script 1 is in progress...");
+}
+
+//function for button 2
+function f2(){
+    alert("Script 2 is in progress...");
+}
+
+//function for button 3
+function f3(){
+    alert("Script 3 is in progress...");
+}
+
+//function for button 4
+function f4(){
+    alert("Script 4 is in progress...");
+}
+
+//function for button 5
+function f5(){
+    alert("Script 5 is in progress...");
+}
+
+//function for button 6
+function f6(){
+    alert("Script 6 is in progress...");
+}
+
+//function for button 7
+function f7(){
+    alert("Script 7 is in progress...");
+}
+
+//function for button 8
+function f8(){
+    alert("Script 8 is in progress...");
+}
+
+//function for button 9
+function f9(){
+    alert("Script 9 is in progress...");
+}
