781b6f8e455b39ea4d424f9f74431cfc620fe065
Rudra <rudra.sondhi2@gmail.com>
2021-04-14 00:25:14 -0400

    10

--- scripts/assignment.js
+++ scripts/assignment.js
@@ -144,52 +144,37 @@ function f9() {
 }
 
 function encrypt() {
+    //Function to encrypt a secret message from the user 
     let rawinput = prompt("Enter your message");
     let output = "";
     rawinput = rawinput.split(" "); // First JS string method used. Split the rawinput into an array of substrings (creates words). 
     for (let i = 0; i < rawinput.length; i++) { //Second JS string method used. Length is used to detemine how many times the loop will run.
         if (rawinput[i].length >= 2) {
-            output += 'CMd2h' + (rawinput[i]).slice(2) + 'i5sd' + (rawinput[i][0]) + "ujhb" + (rawinput[i][1]) + " "; //Third JS string method used. Slide only o
+            output += 'CMd2h' + (rawinput[i]).slice(1) + 'i5sd' + (rawinput[i][1]) + (rawinput[i][0]) + "ujhb "; //Third JS string method used. Slice only keeps a specific part of the input
         }
-        else {
-            output += 'kJ7j' + (rawinput[i][0]) + 'm11Hn ';
+        else { 
+            output += (rawinput[i][0]) + 'm11Hn ';
         }
     }
     document.getElementById('output_user').innerHTML = ("Ecrypted Message: " + output);
 }
 
+function decrypt(ciphertext) {
+    //Function to decrypt a secret message from the user 
+    let output = ''; //Output to be filled later 
 
-function decrypt() {
-    let ciphertext = prompt('Enter');
-    let starttext = '';
-    let endtext = '';
-    let output = '';
+    ciphertext = ciphertext.split(" "); //Splits words apart 
 
-    ciphertext = ciphertext.split(' ');
-    console.log (ciphertext[0]);
-    for (let i = 0; i <= ciphertext.length; i++) {
-        console.log (ciphertext[i]);
-        ciphertext[i] = ciphertext[i].replace(/CMd2h/g, '').replace(/m11Hn/g, '').replace(/i5sd/g, '').replace(/ujhb/g, '').replace(/kJ7j/g, '');
-        // ciphertext[i] = ciphertext[i];
-        // ciphertext[i] = ciphertext[i];
-        // ciphertext[i] = ciphertext[i];
-        // ciphertext[i] = ciphertext[i];
+    for (let i = 0; i < ciphertext.length; i = i + 1) {
+        ciphertext[i] = ciphertext[i].replace(/CMd2h/g, '').replace(/m11Hn/g, '').replace(/i5sd/g, '').replace(/ujhb/g, ''); //Replaces filler with blank spaces
 
-        if (ciphertext.length > 2) {
-            starttext = ciphertext[i].slice(-2);
-            endtext = ciphertext[i].slice(0, -3);
-            output = starttext + endtext + ' ';
+        if (ciphertext[i].length >= 2) { //If length is equal to or more than 2 
+
+            output += [ciphertext[i].slice(-1)] + [(ciphertext[i]).slice(0, -2)] + ' '; //Output = First letter (now in the back) + rest of the text
         }
-
-        else if (ciphertext.length = 2) {
-            starttext = ciphertext[i].slice(-2);
-            output = starttext + ' ';
+        else { //If legnth is anything else 
+            output += ciphertext[i][0] + " ";
         }
-
-        else {
-            output = ciphertext[i][0] + ' ';
-        }
-
     }
-    document.getElementById('output_user').innerHTML = ("Decrypted Message: " + output);
+    document.getElementById('output_user').innerHTML = ("Decrypted Message: " + output); //Output to user throught given text box
 }
